6aab6b614b53757f8c7d4fc3e3e968bab93a5b1d
EWaterman <evanwaterman@hotmail.com>
2016-10-25 22:43:11 -0700

    changed to single page app. added html editting page

---
+++ js/on-btn-press.js
@@ -0,0 +1,57 @@
+// File for all button presses that show page content
+
+// Dev Content
+$('#dev-btn').click(function() {
+    $('#dev-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Close Dev Content
+$('#dev-close').click(function() {
+    $('#dev-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Travel Content
+$('#travel-btn').click(function() {
+    $('#travel-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Close Travel Content
+$('#travel-close').click(function() {
+    $('#travel-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Personal Content
+$('#personal-btn').click(function() {
+    $('#personal-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Close Personal Content
+$('#personal-close').click(function() {
+    $('#personal-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Personal Content
+$('#turn-btn').click(function() {
+    $('#css-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
+
+// Close Personal Content
+$('#css-close').click(function() {
+    $('#css-content').toggle('slow', function() {
+        // Animation complete.
+    });
+});
