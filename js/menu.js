c4f7dfbb671b733179b56a80aaaafb3f8764b0c4
EWaterman <evanwaterman@hotmail.com>
2017-01-25 20:21:37 -0800

    personal page

--- js/menu.js
+++ js/menu.js
@@ -5,18 +5,22 @@ $(document).ready(function() {
     var active1 = false;
     var active2 = false;
     var active3 = false;
+    var active4 = false;
 
     $('.menu').click(function() {
 
         if (!active1) $(this).find('.menu-button1').css({'transform': 'translate(0px,120px)'});
         else $(this).find('.menu-button1').css({'transform': 'none'});
-        if (!active2) $(this).find('.menu-button2').css({'transform': 'translate(90px,90px)'});
+        if (!active2) $(this).find('.menu-button2').css({'transform': 'translate(57px,100px)'});
         else $(this).find('.menu-button2').css({'transform': 'none'});
-        if (!active3) $(this).find('.menu-button3').css({'transform': 'translate(120px,0px)'});
+        if (!active3) $(this).find('.menu-button3').css({'transform': 'translate(103px,59px)'});
         else $(this).find('.menu-button3').css({'transform': 'none'});
+        if (!active4) $(this).find('.menu-button4').css({'transform': 'translate(120px,0px)'});
+        else $(this).find('.menu-button4').css({'transform': 'none'});
         active1 = !active1;
         active2 = !active2;
         active3 = !active3;
+        active4 = !active4;
 
     });
 });
